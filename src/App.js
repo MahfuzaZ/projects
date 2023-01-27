fdfe983b565d76ffca8d2bc8dca281b323df0d17
Abdujabbarof <abdujabborov6172@gmail.com>
2023-01-27 22:10:13 +0500

    updated by Abdulloh

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
-import {Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import Page2 from "./pages/First"
 import Registration from "./pages/Registration"
 import Login from './pages/Login';
+import {Route, Routes} from 'react-router-dom'
+
 function App() {
   return (
     <>
-         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/first' element={<Page2/>}/>
-          <Route path='/registration' element={<Registration/>}/> 
-          <Route path='/login' element={<Login/>}/>
-         </Routes>
-      
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/page2' element={<Page2 />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Registration />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </>
   );
 }
